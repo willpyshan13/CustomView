41fcc4086b68ac67d1c4687c9eb76c96f7024742
will.peng <545512533@qq.com>
2017-04-22 00:12:20 +0800

    完善技能图界面绘制

--- baselibrary/src/main/java/will/baselibrary/widget/SignView.java
+++ baselibrary/src/main/java/will/baselibrary/widget/SignView.java
@@ -18,6 +18,7 @@ import android.view.View;
  */
 public class SignView extends View {
     private static final String TAG = "SignView";
+    private int mLength = 400;
     private Paint paint = new Paint();
     private Path mPath = new Path();
     private Canvas mCanvas;
@@ -89,9 +90,52 @@ public class SignView extends View {
         super.onLayout(changed, left, top, right, bottom);
     }
 
+    //画线跟画圈
+    private void drawCircle(Canvas canvas){
+        canvas.translate(width/2,height/2);
+        int height = (int) (Math.sqrt(3)/2*mLength);
+
+        //画线
+        canvas.drawLine(-mLength,0,mLength,0,paint);
+        canvas.drawLine(-mLength/2,height,mLength/2,-height,paint);
+        canvas.drawLine(mLength/2,height,-mLength/2,-height,paint);
+
+        //画方框
+        for(int i = 0;i<8;i++){
+            mPath.reset();
+            int length = mLength-i*50;
+            height = (int) (Math.sqrt(3)/2*length);
+            mPath.moveTo(-length,0);
+            mPath.lineTo(-length/2,height);
+            mPath.lineTo(length/2,height);
+            mPath.lineTo(length,0);
+            mPath.lineTo(length/2,-height);
+            mPath.lineTo(-length/2,-height);
+            mPath.close();
+            canvas.drawPath(mPath,paint);
+        }
+
+        //画技能图
+        paint.setColor(Color.BLUE);
+        paint.setStyle(Paint.Style.FILL_AND_STROKE);
+        paint.setAlpha(50);
+        mPath.moveTo(-333,0);
+        mPath.lineTo(-123,122);
+        mPath.lineTo(123,123);
+        mPath.lineTo(201,0);
+        mPath.lineTo(150,-90);
+        mPath.lineTo(-123,-132);
+        mPath.close();
+        canvas.drawPath(mPath,paint);
+
+
+    }
+
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        drawCircle(canvas);
+
 //        mCanvas = canvas;
 //        mCanvas.drawPoint(200, 200, paint);     //在坐标(200,200)位置绘制一个点
 //        mCanvas.drawPoints(new float[]{          //绘制一组点，坐标位置由float数组指定
@@ -99,30 +143,32 @@ public class SignView extends View {
 //                500,600,
 //                500,700
 //        },paint);
-        canvas.translate(width/2,height/2);
-
-        canvas.drawLine(-200,0,200,0,paint);
-        canvas.drawLine(-100,200,100,-200,paint);
-        canvas.drawLine(100,200,-100,-200,paint);
-
-//        mPath.addRect(-100,-100,100,100, Path.Direction.CW);
-        mPath.moveTo(-200,0);
-        mPath.lineTo(-100,200);
-        mPath.lineTo(100,200);
-        mPath.lineTo(200,0);
-        mPath.lineTo(100,-200);
-        mPath.lineTo(-100,-200);
-        mPath.close();
-//        Path path = new Path();
+//        canvas.translate(width/2,height/2);
 //
-//        path.addRect(-200,-200,200,200, Path.Direction.CW);
+//        int height = (int) (Math.sqrt(3)/2*200);
+//        canvas.drawLine(-200,0,200,0,paint);
+//        canvas.drawLine(-100,height,100,-height,paint);
+//        canvas.drawLine(100,height,-100,-height,paint);
 //
-//        mPath.addPath(path,0,0);
-//        Path path1 = new Path();
-//        path1.addRect(-50,-50,50,50, Path.Direction.CW);
-////        path1.setLastPoint(0,50);
-//        mPath.addPath(path1,0,0);
-        canvas.drawPath(mPath,paint);
+////        mPath.addRect(-100,-100,100,100, Path.Direction.CW);
+//        mPath.moveTo(-200,0);
+//
+//        mPath.lineTo(-100,height);
+//        mPath.lineTo(100,height);
+//        mPath.lineTo(200,0);
+//        mPath.lineTo(100,-height);
+//        mPath.lineTo(-100,-height);
+//        mPath.close();
+////        Path path = new Path();
+////
+////        path.addRect(-200,-200,200,200, Path.Direction.CW);
+////
+////        mPath.addPath(path,0,0);
+////        Path path1 = new Path();
+////        path1.addRect(-50,-50,50,50, Path.Direction.CW);
+//////        path1.setLastPoint(0,50);
+////        mPath.addPath(path1,0,0);
+//        canvas.drawPath(mPath,paint);
     }
 
     @Override
